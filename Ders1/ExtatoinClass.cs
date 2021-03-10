a04c8431b6cdb6026defbd4642c4e221ff1225c2
İftixar <iftixarbaba5@gmail.com>
2021-03-10 13:02:59 +0400

    Add project files.

---
+++ Ders1/ExtatoinClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders1
+{
+   public static class ExtatoinClass
+    {//objectin yaratmadan cagirmaq ucun bele metodlar  istifade edilir 
+        public static void Show(this object str)//bunlara extantion class lar deil ve mortezenin icine hansi tipe aid olmasi yazilir
+        {
+            Console.WriteLine(str);
+        }
+
+    }
+}
---
+++ Ders1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ders1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Salam";
+            str.Show();
+        }
+    }
+}
