6ce597ea57c1eff278170684edb2babc30216829
İftixar <iftixarbaba5@gmail.com>
2021-03-10 13:17:46 +0400

    ders3

--- Ders1/ExtatoinClass.cs
+++ Ders1/ExtatoinClass.cs
@@ -9,7 +9,8 @@ namespace Ders1
    public static class ExtatoinClass
     {//objectin yaratmadan cagirmaq ucun bele metodlar  istifade edilir 
         public static void Show(this object str)//bunlara extantion class lar deil ve mortezenin icine hansi tipe aid olmasi yazilir
-        {
+        {//                        this object yerine istelinen tipde yazila biler 
+            //                        burda her bir tipler objectden toredyi ucun object yazanda hammina aid ola bilir
             Console.WriteLine(str);
         }
 
---
+++ Ders2/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders2
+{
+   public class Class1:Class2
+    {//                    buna encapsulation deyilir
+        public void Show()// bir birinen toreyen classlarda iki eyni adda metodlar olsa onda onlarda biri
+            //                new birini ise base olaraq gosterilmelidir
+        {
+            base.Show();
+            Console.WriteLine("salam");
+        }
+    }
+}
